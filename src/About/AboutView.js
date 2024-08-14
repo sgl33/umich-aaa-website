bf643fe553069034db4c03816ee5b585e1ca30c5
sgl33 <awesomeguy4k@gmail.com>
2024-08-14 15:10:03 -0400

    Add Twitter card meta and update club logo

--- src/About/AboutView.js
+++ src/About/AboutView.js
@@ -36,7 +36,7 @@ export default function AboutView() {
                         <div className="content-middle">
                             <h2>What is AAA?</h2>
                             <p>
-                                Anthro Art Association (AAA) is a registered voluntary student organization (VSO) at the University oßf Michigan - Ann Arbor.
+                                Anthro Art Association (AAA) is a registered voluntary student organization (VSO) at the University of Michigan - Ann Arbor.
                             </p>
                             <p>
                                 We are a member club of the Anthromorphic Animal Appreciation Association, a global federation of university furry clubs including clubs at UC Berkeley, UIUC, Georgia Tech, and more.
